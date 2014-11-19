87c7b792bcbf8bc0e7414ccbbec9b45434b56a07
shervin <shervin.hz@live.com>
2014-11-19 15:48:12 +0330

    details are changed for Nikko

--- db/nikko_db.sql
+++ db/nikko_db.sql
@@ -1,11 +1,11 @@
 -- phpMyAdmin SQL Dump
--- version 4.1.6
+-- version 4.0.10deb1
 -- http://www.phpmyadmin.net
 --
--- Host: 127.0.0.1
--- Generation Time: Aug 17, 2014 at 08:45 AM
--- Server version: 5.6.16
--- PHP Version: 5.5.9
+-- Host: localhost
+-- Generation Time: Nov 19, 2014 at 03:46 PM
+-- Server version: 5.5.37-0ubuntu0.14.04.1
+-- PHP Version: 5.5.9-1ubuntu4
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 SET time_zone = "+00:00";
@@ -17,10 +17,8 @@ SET time_zone = "+00:00";
 /*!40101 SET NAMES utf8 */;
 
 --
--- Database: `keeper_db2`
+-- Database: `nikko_db`
 --
---CREATE DATABASE IF NOT EXISTS `cr_db` DEFAULT CHARACTER SET utf8 COLLATE utf8_general_ci;
--- USE `fresh_db`;
 
 -- --------------------------------------------------------
 
@@ -28,7 +26,6 @@ SET time_zone = "+00:00";
 -- Table structure for table `members`
 --
 
-DROP TABLE IF EXISTS `members`;
 CREATE TABLE IF NOT EXISTS `members` (
   `id` bigint(20) NOT NULL AUTO_INCREMENT,
   `name` varchar(100) DEFAULT NULL,
@@ -40,7 +37,40 @@ CREATE TABLE IF NOT EXISTS `members` (
   `email` varchar(150) DEFAULT NULL,
   `active` tinyint(4) NOT NULL DEFAULT '1',
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB  DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB  DEFAULT CHARSET=utf8 AUTO_INCREMENT=3 ;
+
+--
+-- Dumping data for table `members`
+--
+
+INSERT INTO `members` (`id`, `name`, `familyname`, `username`, `password`, `type`, `manager_id`, `email`, `active`) VALUES
+(1, 'Admin', 'Admin', 'admin', '81dc9bdb52d04dc20036dbd8313ed055', 0, 0, 'admin@admin.com', 1),
+(2, 'Test', 'User', 'test', '81dc9bdb52d04dc20036dbd8313ed055', 1, 1, 'a@a.com', 1);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `services`
+--
+
+CREATE TABLE IF NOT EXISTS `services` (
+  `id` int(10) unsigned NOT NULL AUTO_INCREMENT,
+  `title` varchar(150) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 AUTO_INCREMENT=1 ;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `service_alias`
+--
+
+CREATE TABLE IF NOT EXISTS `service_alias` (
+  `id` int(10) unsigned NOT NULL AUTO_INCREMENT,
+  `service_id` int(10) unsigned NOT NULL,
+  `alias` varchar(150) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 AUTO_INCREMENT=1 ;
 
 -- --------------------------------------------------------
 
@@ -48,13 +78,32 @@ CREATE TABLE IF NOT EXISTS `members` (
 -- Table structure for table `tbl_members_roles`
 --
 
-DROP TABLE IF EXISTS `tbl_members_roles`;
 CREATE TABLE IF NOT EXISTS `tbl_members_roles` (
   `id` bigint(20) unsigned NOT NULL AUTO_INCREMENT,
   `member_id` bigint(20) unsigned NOT NULL,
   `role_id` bigint(20) unsigned NOT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB  DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 AUTO_INCREMENT=1 ;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `tbl_member_types`
+--
+
+CREATE TABLE IF NOT EXISTS `tbl_member_types` (
+  `id` int(10) unsigned NOT NULL AUTO_INCREMENT,
+  `title` varchar(150) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB  DEFAULT CHARSET=utf8 AUTO_INCREMENT=3 ;
+
+--
+-- Dumping data for table `tbl_member_types`
+--
+
+INSERT INTO `tbl_member_types` (`id`, `title`) VALUES
+(1, 'administrator'),
+(2, 'user');
 
 -- --------------------------------------------------------
 
@@ -62,12 +111,19 @@ CREATE TABLE IF NOT EXISTS `tbl_members_roles` (
 -- Table structure for table `tbl_objects`
 --
 
-DROP TABLE IF EXISTS `tbl_objects`;
 CREATE TABLE IF NOT EXISTS `tbl_objects` (
   `id` bigint(20) unsigned NOT NULL AUTO_INCREMENT,
   `title` varchar(255) NOT NULL,
+  `creator_id` bigint(20) unsigned DEFAULT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB  DEFAULT CHARSET=utf8 ;
+) ENGINE=InnoDB  DEFAULT CHARSET=utf8 AUTO_INCREMENT=2 ;
+
+--
+-- Dumping data for table `tbl_objects`
+--
+
+INSERT INTO `tbl_objects` (`id`, `title`, `creator_id`) VALUES
+(1, 'forum', 0);
 
 -- --------------------------------------------------------
 
@@ -75,15 +131,16 @@ CREATE TABLE IF NOT EXISTS `tbl_objects` (
 -- Table structure for table `tbl_permissions`
 --
 
-DROP TABLE IF EXISTS `tbl_permissions`;
 CREATE TABLE IF NOT EXISTS `tbl_permissions` (
   `id` bigint(20) unsigned NOT NULL AUTO_INCREMENT,
   `object_id` bigint(20) unsigned NOT NULL,
   `right_id` bigint(20) unsigned NOT NULL,
   `role_id` bigint(20) unsigned NOT NULL,
+  `table_name` varchar(150) NOT NULL,
+  `column_name` varchar(150) NOT NULL,
   `create_date` date NOT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB  DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 AUTO_INCREMENT=1 ;
 
 -- --------------------------------------------------------
 
@@ -91,30 +148,42 @@ CREATE TABLE IF NOT EXISTS `tbl_permissions` (
 -- Table structure for table `tbl_rights`
 --
 
-DROP TABLE IF EXISTS `tbl_rights`;
 CREATE TABLE IF NOT EXISTS `tbl_rights` (
   `id` int(10) unsigned NOT NULL AUTO_INCREMENT,
   `title` varchar(150) NOT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB  DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB  DEFAULT CHARSET=utf8 AUTO_INCREMENT=6 ;
+
+--
+-- Dumping data for table `tbl_rights`
+--
+
 INSERT INTO `tbl_rights` (`id`, `title`) VALUES
 (1, 'full'),
 (2, 'view'),
 (3, 'add'),
 (4, 'delete'),
 (5, 'update');
+
 -- --------------------------------------------------------
 
 --
 -- Table structure for table `tbl_roles`
 --
 
-DROP TABLE IF EXISTS `tbl_roles`;
 CREATE TABLE IF NOT EXISTS `tbl_roles` (
   `id` bigint(20) unsigned NOT NULL AUTO_INCREMENT,
   `title` varchar(255) NOT NULL,
+  `creator_id` bigint(20) unsigned DEFAULT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB  DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB  DEFAULT CHARSET=utf8 AUTO_INCREMENT=2 ;
+
+--
+-- Dumping data for table `tbl_roles`
+--
+
+INSERT INTO `tbl_roles` (`id`, `title`, `creator_id`) VALUES
+(1, 'test_role', 1);
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
